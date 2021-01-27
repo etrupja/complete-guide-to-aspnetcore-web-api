ba8c414f81430aef1d5991bc404972d0b7f09f7c
Ervis Trupja <ervistrupja@outlook.com>
2021-01-27 19:18:33 +0100

    Added book model

---
+++ my-books/Data/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Data.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public int? Rate { get; set; }
+        public string Genre { get; set; }
+        public string Author { get; set; }
+        public string CoverUrl { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
