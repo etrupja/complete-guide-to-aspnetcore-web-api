ebd3d3240fe78c2522536d14bb49d2eaae2b1a24
Ervis Trupja <ervistrupja@outlook.com>
2021-01-30 16:57:24 +0100

    Added New Controller [BooksController]

---
+++ my-books/Controllers/BooksController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+    }
+}
