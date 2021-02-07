8451ce7bd8b26d50610d8880bd0609aec0e5a910
Ervis Trupja <ervistrupja@outlook.com>
2021-02-07 15:14:48 +0100

    Added One-To-Many Relationship

--- my-books/Data/Models/Book.cs
+++ my-books/Data/Models/Book.cs
@@ -17,5 +17,9 @@ namespace my_books.Data.Models
         public string Author { get; set; }
         public string CoverUrl { get; set; }
         public DateTime DateAdded { get; set; }
+
+        //Navigation Properties
+        public int PublisherId { get; set; }
+        public Publisher Publisher { get; set; }
     }
 }
