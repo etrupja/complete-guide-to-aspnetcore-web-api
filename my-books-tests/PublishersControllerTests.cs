9191ed194a18e0e1b7229ecdea866c948feb9db7
Ervis Trupja <ervistrupja@outlook.com>
2021-04-06 14:47:30 +0200

    Unit tested DeletePublisherById

--- my-books-tests/PublishersControllerTests.cs
+++ my-books-tests/PublishersControllerTests.cs
@@ -106,6 +106,28 @@ namespace my_books_tests
             Assert.That(actionResult, Is.TypeOf<BadRequestObjectResult>());
         }
 
+        [Test, Order(6)]
+        public void HTTPDELETE_DeletePublisherById_ReturnsOk_Test()
+        {
+            int publisherId = 6;
+
+            IActionResult actionResult = publishersController.DeletePublisherById(publisherId);
+
+            Assert.That(actionResult, Is.TypeOf<OkResult>());
+
+        }
+
+
+        [Test, Order(7)]
+        public void HTTPDELETE_DeletePublisherById_ReturnsBadRequest_Test()
+        {
+            int publisherId = 6;
+
+            IActionResult actionResult = publishersController.DeletePublisherById(publisherId);
+
+            Assert.That(actionResult, Is.TypeOf<BadRequestObjectResult>());
+        }
+
         [OneTimeTearDown]
         public void CleanUp()
         {
