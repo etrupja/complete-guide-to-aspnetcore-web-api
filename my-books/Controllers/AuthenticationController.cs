bf35ad96a8c5df3d906bc5a5e7f4895e1ff09604
Ervis Trupja <ervistrupja@outlook.com>
2021-04-26 14:12:05 +0200

    Added authentication controller

---
+++ my-books/Controllers/AuthenticationController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using my_books.Data;
+using my_books.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            AppDbContext context,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+            _configuration = configuration;
+        }
+
+
+    }
+}
